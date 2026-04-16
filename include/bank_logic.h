4142c3bee2d6b53c2e52da465c6fcad1a95dda1b
Mohamed Ishag <mohamed.ishag.dev@gmail.com>
2026-04-16 23:01:53 +0200

    Create bank_logic.h

---
+++ include/bank_logic.h
@@ -0,0 +1,17 @@
+#pragma once
+
+/**
+ * @file bank_logic.h
+ * @author Mohamed Ishag
+ * @brief Header file containing bank system operations and structures.
+ * @version 1.0
+ * @date 2026-04-16
+ */
+
+#ifndef BANK_LOGIC_H
+#define BANK_LOGIC_H
+
+void Login();
+
+
+#endif
